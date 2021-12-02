e8c40b3e43f9635123f1168452a181320198f893
Jordan <jtate1166@gmail.com>
2021-12-02 11:27:46 -0500

    Allow user to create custom sized grid after clicking the clear button

--- script.js
+++ script.js
@@ -7,18 +7,43 @@ clearButton.textContent = "Clear";
 
 flexContainer.appendChild(clearButton);
 
-for (let i = 0; i < 256; i++) {
-  let box = document.createElement("div");
-  box.classList.add("grid-box");
-  gridContainer.appendChild(box);
-  box.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "blue";
-  });
-}
+createGrid();
 
 clearButton.addEventListener("click", () => {
   let box = document.getElementsByClassName("grid-box");
   Array.from(box).forEach((gridBox) => {
     gridBox.style.backgroundColor = "";
   });
+  let newGrid = Number(
+    prompt(
+      "How many squares per side? Example: If you choose 64, a 64 by 64 square grid will be created."
+    )
+  );
+  removeElementsByClass("grid-box");
+  createGrid(newGrid);
 });
+
+function createGrid(number = 16) {
+  for (let i = 0; i < number; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    row.id = `row-${i + 1}`;
+    gridContainer.appendChild(row);
+    for (let j = 0; j < number; j++) {
+      let box = document.createElement("div");
+      box.classList.add("grid-box");
+      box.id = `box-${j + 1}`;
+      row.appendChild(box);
+      box.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "blue";
+      });
+    }
+  }
+}
+
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
