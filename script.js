4246c237a802f42b0eae749ab1fa08cc44d44072
Jordan <jtate1166@gmail.com>
2021-12-02 10:41:13 -0500

    Add button that resets the grid

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+let flexContainer = document.getElementById("flex-container");
 let gridContainer = document.getElementById("grid-container");
+
+let clearButton = document.createElement("button");
+clearButton.classList.add("clear-button");
+clearButton.textContent = "Clear";
+
+flexContainer.appendChild(clearButton);
 
 for (let i = 0; i < 256; i++) {
   let box = document.createElement("div");
@@ -8,3 +15,10 @@ for (let i = 0; i < 256; i++) {
     e.target.style.backgroundColor = "blue";
   });
 }
+
+clearButton.addEventListener("click", () => {
+  let box = document.getElementsByClassName("grid-box");
+  Array.from(box).forEach((gridBox) => {
+    gridBox.style.backgroundColor = "";
+  });
+});
