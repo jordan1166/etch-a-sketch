345f5676b2bd845687d5475058076718a64081b0
Jordan <jtate1166@gmail.com>
2021-12-02 10:07:11 -0500

    Add hover effect so grid blocks change color when cursor passes over them

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ for (let i = 0; i < 256; i++) {
   let box = document.createElement("div");
   box.classList.add("grid-box");
   gridContainer.appendChild(box);
+  box.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "blue";
+  });
 }
